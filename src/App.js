be802489a07926ecbaef950745c0621303369453
zezinho1234 <omarcosvini18@gmail.com>
2020-01-13 16:29:33 -0300

    novo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Form from "./app_components/form.component";
 import Weather from "./app_components/weather.component";
 
-
-// git project https://github.com/erikflowers/weather-icons
 import "weather-icons/css/weather-icons.css";
 
 const Api_Key = "429736441cf3572838aa10530929f7cd";
